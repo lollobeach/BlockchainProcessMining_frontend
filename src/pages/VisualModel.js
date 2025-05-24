24362d412cf00482f15ac0a1d7e2a77f46d2c378
LolloLauz <lorenzo.luzi@cooder.it>
2025-05-24 14:29:45 +0200

    Bugs fixing

--- src/pages/VisualModel.js
+++ src/pages/VisualModel.js
@@ -40,227 +40,6 @@ const CardContentNoPadding = styled(CardContent)`
 `;
 
 const NetworkGraph = () => {
-    // const { results, setResults } = useDataContext();
-    // const [loading, setLoading] = useState(false);
-    // const [graphData, setGraphData] = useState(null);
-    // const [selectedNode, setSelectedNode] = useState(null);
-    // const [edgesArray, setEdgesArray] = useState([]);
-    // const [nodesSet, setNodesSet] = useState([]);
-    // const [objectsTypesItem, setObjectsTypesItem] = useState([]);
-    // const [visibleNodes, setVisibleNodes] = useState(null);
-
-
-    // const handleFileChange = (e) => {
-    //     const fileReader = new FileReader();
-    //     fileReader.onload = (event) => {
-    //         const content = event.target.result;
-    //         try {
-    //             setResults(JSON.parse(content));
-    //         } catch (err) {
-    //             console.error("Invalid JSON file");
-    //         }
-    //     };
-    //     if (e.target.files[0]) {
-    //         fileReader.readAsText(e.target.files[0]);
-    //     }
-    //     e.target.value = null;
-    // };
-
-    // const handleAddObjectType = () => {
-    //     setObjectsTypesItem([...objectsTypesItem, { nameFrom: "From", nameTo: "To", from: [], to: [] }]);
-    // };
-    // // const handleSearch = (query) => {
-    // //     const edgesFiltered = edgesArray.filter(edge => edge.value === parseInt(query));
-    // //     const nodesFiltered = [];
-    // //     edgesFiltered.forEach((edge) => {
-    // //         if(edge.value === parseInt(query)) {
-    // //             nodesSet.forEach((node)=>{
-    // //                 if(edge.from === node.id || edge.to === node.id){
-    // //                     nodesFiltered.push(node);
-    // //                     // graphData.setNodeAttribute(node.id, 'hidden', false);   
-    // //                 }
-
-    // //             })
-
-    // //         }
-    // //     })
-    // //     // setGraphData(graphData);
-    // //     setGraphData(createGraph(nodesFiltered, edgesFiltered));
-    // // };
-    // const handleSearch = (query) => {
-    //     const visible = new Set();
-    //     const edgesFiltered = edgesArray.filter(edge => edge.value === parseInt(query));
-    //     edgesFiltered.forEach((edge) => {
-    //         nodesSet.forEach((node) => {
-    //             if(edge.from === node.id || edge.to === node.id){
-                    
-    //             }else{  
-    //                 graphData.setNodeAttribute(edge.from, 'hidden', true);
-    //                 graphData.setNodeAttribute(edge.to, 'hidden', true);
-    //             }
-    //         })
-    //     });
-    //     setVisibleNodes(visible);
-
-    //   };
-      
-    // const createGraph = (nodes, edges) => {
-    //     const graph = new MultiDirectedGraph();
-    //     setNodesSet(nodes)
-    //     // Add nodes to the graph
-    //     nodes.forEach((node) => {
-    //         graph.addNode(node.id, {
-    //             ...node,
-    //         });
-    //     });
-    //     setEdgesArray(edges);
-    //     // Determine max and min edge values
-        // const edgeValues = edges.map(edge => edge.value);
-        // const maxEdgeValue = Math.max(...edgeValues);
-        // const minEdgeValue = Math.min(...edgeValues);
-        // // Scale edge values to a smaller range (e.g., 1 to 10)
-        // const scaleEdgeValue = (value) => {
-        //     if (maxEdgeValue === minEdgeValue) return 5; // Avoid division by zero
-        //     return ((value - minEdgeValue) / (maxEdgeValue - minEdgeValue)) * 9 + 1;
-        // };
-
-    //     // Add edges with scaled values
-    //     edges.forEach((edge) => {
-    //         graph.addEdgeWithKey(edge.id,edge.from, edge.to, {
-    //             size: scaleEdgeValue(edge.value),
-    //         });
-    //     });
-    //     // Apply ForceAtlas2 layout
-    //     forceAtlas2.assign(graph, { iterations: 100, settings: { gravity: 0, scalingRatio: 10 } });
-
-    //     return graph;
-    // };
-    // const generateGraph = () => {
-    //     setLoading(true);
-    //     _generateGraph(results, objectsTypesItem).then((response) => {
-    //         setLoading(false);
-    //         const nodes = Array.from(response.data.nodesSet.values());
-    //         const edges = response.data.edgesArray;
-    //         // Add node
-    //        setGraphData(createGraph(nodes, edges));
-    //     }).catch(err => {
-    //         setLoading(false);
-    //         console.error("Error generating graph:", err);
-    //     });
-    // };
-
-    // const LoadGraph = () => {
-    //     const loadGraph = useLoadGraph();
-    //     useEffect(() => {
-    //         if (graphData) {
-    //             console.log("Loading graph data:", graphData);
-    //             // Set the graph data
-    //             loadGraph(graphData);
-    //         }
-    //     }, [graphData,loadGraph]);
-    //     return null;
-    // };
-    // const RefreshOnFilterChange = ({ visibleNodes }) => {
-    //     const sigma = useSigma();
-      
-    //     useEffect(() => {
-
-    //         sigma.refresh(); // Automatically refresh when visibleNodes changes
-    //     }, [visibleNodes, sigma]);
-      
-    //     return null;
-    //   };
-    // return (
-    //     <Box display="flex" flexDirection="column" gap={2}>
-            // <Box display="flex" gap={2}>
-            //     <Button
-            //         component="label"
-            //         variant="contained"
-            //         startIcon={<FileUpload />}
-            //         sx={{ padding: 1, height: "55px" }}
-            //     >
-            //         Upload File
-            //         <HiddenInput type="file" onChange={handleFileChange} />
-            //     </Button>
-
-            //     <Stack marginY={3} height="calc(100vh - 300px)" overflow="auto">
-            //         <CustomTypography>
-            //             &nbsp;&nbsp;&nbsp;"Edges":
-            //             <Button onClick={handleAddObjectType}>
-            //                 <AddBoxIcon sx={{ fontSize: 30 }} />
-            //             </Button>
-            //         </CustomTypography>
-            //         {objectsTypesItem.map((objectType, index) => (
-            //             <KeyType
-            //                 key={`object-type-${index}`}
-            //                 nameFrom={objectType.nameFrom}
-            //                 nameTo={objectType.nameTo}
-            //                 objectToSet={objectType}
-            //                 index={index}
-            //                 setObjectsTypesItem={setObjectsTypesItem}
-            //             />
-            //         ))}
-            //     </Stack>
-
-            //     <Button
-            //         variant="contained"
-            //         onClick={generateGraph}
-            //         sx={{ padding: 1, height: "55px" }}
-            //     >
-            //         {loading ? <CircularProgress size={24} color="inherit" /> : "Generate Graph"}
-            //     </Button>
-            // </Box>
-    //         <Box display="flex" flexDirection="column" gap={2}>
-    //             <Typography variant="h6">Select Edge Value</Typography>
-                // <TextField
-                    // select
-                    // SelectProps={{ native: true }}
-                    // defaultValue=""
-                    // onChange={(e) => handleSearch(e.target.value)}
-                //     sx={{ width: 100 }}
-                // >
-                //     <option value="">All</option>
-                //     {graphData &&
-                //         Array.from(new Set(edgesArray.map(edge => edge.value)))
-                //             .sort((a, b) => a - b)
-                //             .map((value, index) => (
-                //                 <option key={index} value={value}>
-                //                     {value}
-                //                 </option>
-                //             ))}
-                // </TextField>
-    //         </Box>
-
-    //         <Box sx={{ height: 500, width: "100%", mt: 2 }}>
-    //             <SigmaContainer
-    //                 style={{ height: "100%", width: "100%" }}
-    //                 settings={{
-    //                     labelRenderedSizeThreshold: 10,
-    //                     defaultNodeColor: "#3399FF",
-    //                 }}
-    //                 >
-    //                 <LoadGraph />
-    //                 <RefreshOnFilterChange visibleNodes={visibleNodes} />
-    //             </SigmaContainer>
-    //         </Box>
-
-    //         {selectedNode && (
-    //             <Card>
-    //                 <CardContentNoPadding
-    //                     sx={{
-    //                         height: "calc(100% - 112px)",
-    //                         overflowY: "auto",
-    //                         overflowX: "auto",
-    //                         whiteSpace: "pre",
-    //                     }}
-    //                 >
-    //                     <Typography variant="h6">Selected Node Details</Typography>
-    //                     <JsonView value={selectedNode} style={{ fontSize: '14px' }} width="100%" />
-    //                 </CardContentNoPadding>
-    //             </Card>
-    //         )}
-    //     </Box>
-    // );
     const [selectedNode, setSelectedNode] = useState(null);
     const { results, setResults } = useDataContext();
     const [running, setRunning] = useState(false);
@@ -269,6 +48,7 @@ const NetworkGraph = () => {
     const [colorLegend,setColorLegend]=useState([]);
     const [visibleNodesCount, setVisibleNodeCount]=useState(0);
     const [startLayout,setStartLayout]=useState(false);
+    const [visibleEdgeCount, setVisibleEdgeCount]=useState(0);
   const [graphData, setGraphData] = useState({
     nodes: [
     ],
@@ -283,10 +63,11 @@ const NetworkGraph = () => {
     setSelectedNode(value === "" ? null : value);
   };
   const handleVisibleNodeCount=(count)=>{
-    // console.log("count",count)
     setVisibleNodeCount(count);
   }
-  
+  const hadleVisibleEdgeCount=(count)=>{
+    setVisibleEdgeCount(count);
+  }
 
   const handleLoadGraph = () => {
     const newNodes = [
@@ -329,98 +110,29 @@ const NetworkGraph = () => {
   }
     const generateGraph = () => {
         setLoading(true);
-        _generateGraph(results, objectsTypesItem).then((response) => {
-            setLoading(false);
-            const nodes = Array.from(response.data.nodesSet.values());
-            const edges = response.data.edgesArray;
-            // Add node
-            const newNodes=nodes.map((obj,index) => ({
-                key:obj.id,
-                attributes: {
-                    label: `${obj.label}`,
-                    size: 10,
-                    details:obj.details,
-                    keyUsed:obj.keyUsed,
-                    color: obj.color,
-                    x: Math.random() * 100,
-                    y: Math.random() * 100,
-                },
-            }))
-            const colorLegendData = new Set();
-            const checkColorElement = (color) => {
-                let flag=true;
-                colorLegendData.forEach((element) => {
-                  if (element.color===color) {
-                    flag= false;
-                  }
-                }); 
-                return flag;
-            }
-            newNodes.forEach((node) => {
-                if (node.attributes.color ) {
-                  if(checkColorElement(node.attributes.color)){
-                    colorLegendData.add({color:node.attributes.color,keyAssigned:node.attributes.keyUsed});
-                  }
-                }
-              });
+        const startTime = performance.now();
+        if(objectsTypesItem.length !== 0){
+          _generateGraph(results, objectsTypesItem).then((response) => {
+              setLoading(false);
+              const nodes = Array.from(response.data.nodes.values());
+              const edges = response.data.edges;
+              setColorLegend(response.data.colorLegend);
+              setEdgeFilter(response.data.edgeFilter);
+              setGraphData({nodes:nodes, edges:edges});
+              const endTime = performance.now(); 
+              console.log(`Graph creation took ${(endTime - startTime).toFixed(2)} ms`);
 
-            setColorLegend(colorLegendData);
-            // setColorLegend(colorLegendData);
-            const edgeValues = edges.map(edge => edge.value);
-            setEdgeFilter(edgeValues);
-            const maxEdgeValue = Math.max(...edgeValues);
-            const minEdgeValue = Math.min(...edgeValues);
-            // Scale edge values to a smaller range (e.g., 1 to 10)
-            const scaleEdgeValue = (value) => {
-                if (maxEdgeValue === minEdgeValue) return 1; // Avoid division by zero
-                return ((value - minEdgeValue) / (maxEdgeValue - minEdgeValue)) * 4 + 1;
-            }
-            const newEdges=edges.map((obj,index) => ({
-                key:obj.id,
-                source: obj.from,
-                target: obj.to,
-                attributes:{
-                    value:obj.value,
-                    size: scaleEdgeValue(obj.value),
-                    color: '#3399FF',
-                    x: Math.random() * 100,
-                    y: Math.random() * 100,
-                },
-            }))
-        setGraphData({nodes:newNodes, edges:newEdges});
-
-        setLoading(false);
-        }).catch(err => {
-            setLoading(false);
-            // console.error("Error generating graph:", err);
-        });
+          setLoading(false);
+          }).catch(err => {
+              setLoading(false);
+              console.error("Error generating graph:", err);
+          });
+        }else{
+          setLoading(false)
+          const endTime = performance.now(); 
+          console.log(`Graph creation took ${(endTime - startTime).toFixed(2)} ms`);
+        }
     };
-//   const generateGraph = () => {
-//     setLoading(true);
-
-//     setTimeout(() => {
-//       const newNodes = objectsTypesItem.map((obj, index) => ({
-//         key: `generated_node_${index}`,
-//         attributes: {
-//           label: `${obj.nameFrom} to ${obj.nameTo}`,
-//           size: 15,
-//           color: '#1abc9c',
-//           x: Math.random() * 100,
-//           y: Math.random() * 100,
-//         },
-//       }));
-
-//       const newEdges = newNodes.map((node, index) => ({
-//         key: `generated_edge_${index}`,
-//         source: newNodes[index % newNodes.length].key,
-//         target: newNodes[(index + 1) % newNodes.length].key,
-//         attributes: {},
-//       }));
-
-//       setGraphData({ nodes: newNodes, edges: newEdges });
-//       setLoading(false);
-//     }, 1000); // fake loading
-//   };
 
   return (
     <div style={{ height: '100%', width: '100%'}}>
@@ -475,6 +187,7 @@ const NetworkGraph = () => {
                     ))}
         </TextField>
        <Typography variant="h6">Visible Nodes: {visibleNodesCount}</Typography>
+        <Typography variant="h6">Visible Edges: {visibleEdgeCount}</Typography>
       <Box display="flex" flexDirection="column" gap={2}>
         <Typography variant="h6">Color Legend</Typography>
         {Array.from(colorLegend).map((legendItem, index) => (
@@ -517,6 +230,7 @@ const NetworkGraph = () => {
         nodeFilter={nodeFilter}
         onNodeSelected={handleNodeSelected}
         onVisibleNodeCount={handleVisibleNodeCount}
+        onVisibleEdgeCount={hadleVisibleEdgeCount}
         startLayout={startLayout}
         />
         {/* { graphData &&(
